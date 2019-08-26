0617fafa2995305b92d755ecfb4f00cd6d9be3e0
Daniel Lewis <daniellewis777@gmail.com>
2019-08-26 01:39:52 +0100

    added pi convergents

--- src/data/number.data.ts
+++ src/data/number.data.ts
@@ -57,6 +57,151 @@ export const numberData = {
     reason: 'YAH * YAHWEH * EL * ELOHIM'
   },
 
+  //  PI convergents
+  333: {
+    number: 333,
+    reason: '333/106 = PI convergent'
+  },
+  106: {
+    number: 106,
+    reason: '333/106 = PI convergent'
+  },
+
+  355: {
+    number: 355,
+    reason: '355/113 = PI convergent'
+  },
+  113: {
+    number: 113,
+    reason: '355/113 = PI convergent'
+  },
+
+  103993: {
+    number: 103993,
+    reason: '103993/33102 = PI convergent'
+  },
+  33102: {
+    number: 33102,
+    reason: '103993/33102 = PI convergent'
+  },
+
+  104348: {
+    number: 104348,
+    reason: '104348/33215 = PI convergent'
+  },
+  33215: {
+    number: 33215,
+    reason: '104348/33215 = PI convergent'
+  },
+
+  208341: {
+    number: 208341,
+    reason: '208341/66317 = PI convergent'
+  },
+  66317: {
+    number: 66317,
+    reason: '208341/66317 = PI convergent'
+  },
+
+  312689: {
+    number: 312689,
+    reason: '312689/99532 = PI convergent'
+  },
+  99532: {
+    number: 99532,
+    reason: '312689/99532 = PI convergent'
+  },
+
+  833719: {
+    number: 833719,
+    reason: '833719/265381 = PI convergent'
+  },
+  265381: {
+    number: 265381,
+    reason: '833719/265381 = PI convergent'
+  },
+
+  1146408: {
+    number: 1146408,
+    reason: '1146408/364913 = PI convergent'
+  },
+  364913: {
+    number: 364913,
+    reason: '1146408/364913 = PI convergent'
+  },
+
+  4272943: {
+    number: 4272943,
+    reason: '4272943/1360120 = PI convergent'
+  },
+  1360120: {
+    number: 1360120,
+    reason: '4272943/1360120 = PI convergent'
+  },
+
+  5419351: {
+    number: 5419351,
+    reason: '5419351/1725033 = PI convergent'
+  },
+  1725033: {
+    number: 1725033,
+    reason: '5419351/1725033 = PI convergent'
+  },
+
+  80143857: {
+    number: 80143857,
+    reason: '80143857/25510582 = PI convergent'
+  },
+  25510582: {
+    number: 25510582,
+    reason: '80143857/25510582 = PI convergent'
+  },
+
+  165707065: {
+    number: 165707065,
+    reason: '165707065/52746197 = PI convergent'
+  },
+  52746197: {
+    number: 52746197,
+    reason: '165707065/52746197 = PI convergent'
+  },
+
+  245850922: {
+    number: 245850922,
+    reason: '245850922/78256779 = PI convergent'
+  },
+  78256779: {
+    number: 78256779,
+    reason: '245850922/78256779 = PI convergent'
+  },
+
+  411557987: {
+    number: 411557987,
+    reason: '411557987/131002976 = PI convergent'
+  },
+  131002976: {
+    number: 131002976,
+    reason: '411557987/131002976 = PI convergent'
+  },
+
+  1068966896: {
+    number: 1068966896,
+    reason: '1068966896/340262731 = PI convergent'
+  },
+  340262731: {
+    number: 340262731,
+    reason: '1068966896/340262731 = PI convergent'
+  },
+
+  2549491779: {
+    number: 2549491779,
+    reason: '2549491779/811528438 = PI convergent'
+  },
+  811528438: {
+    number: 811528438,
+    reason: '2549491779/811528438 = PI convergent'
+  },
+
   // OTHERS
   110: {
     number: 110,
