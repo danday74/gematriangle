641fd4ca240be9e6e33ee800c61383296fb5ad40
Daniel Lewis <daniellewis777@gmail.com>
2019-08-30 04:38:21 +0100

    made color count code better

--- src/app/reusable/color/color-count/color-count.component.ts
+++ src/app/reusable/color/color-count/color-count.component.ts
@@ -34,45 +34,14 @@ export class ColorCountComponent extends DestroyerComponent implements OnInit {
   }
 
   private getColors(allCounters) {
-    const appGrey = filter(allCounters, {color: 'appGrey'})
-    const appRed = filter(allCounters, {color: 'appRed'})
-    const appPink = filter(allCounters, {color: 'appPink'})
-    const appGreen = filter(allCounters, {color: 'appGreen'})
-    const appOrange = filter(allCounters, {color: 'appOrange'})
-    const appBlue = filter(allCounters, {color: 'appBlue'})
-    const appGreyValue = this.getValueSum(appGrey)
-    const appRedValue = this.getValueSum(appRed)
-    const appPinkValue = this.getValueSum(appPink)
-    const appGreenValue = this.getValueSum(appGreen)
-    const appOrangeValue = this.getValueSum(appOrange)
-    const appBlueValue = this.getValueSum(appBlue)
-
-    return {
-      appGrey: {
-        count: appGrey.length,
-        value: appGreyValue
-      },
-      appRed: {
-        count: appRed.length,
-        value: appRedValue
-      },
-      appPink: {
-        count: appPink.length,
-        value: appPinkValue
-      },
-      appGreen: {
-        count: appGreen.length,
-        value: appGreenValue
-      },
-      appOrange: {
-        count: appOrange.length,
-        value: appOrangeValue
-      },
-      appBlue: {
-        count: appBlue.length,
-        value: appBlueValue
-      }
-    }
+    const colors = ['appGrey', 'appRed', 'appPink', 'appGreen', 'appOrange', 'appBlue']
+    return colors.reduce((acc, color) => {
+      acc[color] = {}
+      const counters = filter(allCounters, {color})
+      acc[color].count = counters.length
+      acc[color].value = this.getValueSum(counters)
+      return acc
+    }, {})
   }
 
   private getValueSum(counters: Array<Counter>) {
