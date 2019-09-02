bf640ab72518a8c35e70da39fb93ac7ba5d82076
Daniel Lewis <daniellewis777@gmail.com>
2019-09-02 02:02:38 +0100

    finished off triangle list

--- src/app/routes/triangles-list/triangles-list-routing.module.ts
+++ src/app/routes/triangles-list/triangles-list-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { TrianglesListComponent } from './triangles-list.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    component: TrianglesListComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/routes/triangles-list/triangles-list.component.ts
+++ src/app/routes/triangles-list/triangles-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { triangle } from '../../utils/triangle'
+import { star } from '../../utils/star'
 
 @Component({
   selector: 'app-triangles-list',
@@ -7,7 +10,26 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class TrianglesListComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  numTrianglesListed = 201
+  triangles = []
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    for (let i = 1; i <= this.numTrianglesListed; i++) {
+      const starOfDavid = triangle.housesStarOfDavid(i)
+      this.triangles.push({
+        T: i,
+        perimeter: triangle.perimeter(i),
+        counters: triangle.term(i),
+        starOfDavid,
+        starCounters: star.term(starOfDavid)
+      })
+    }
+  }
+
+  rowClick(n) {
+    this.router.navigateByUrl('triangles/' + n).then()
+  }
 }
