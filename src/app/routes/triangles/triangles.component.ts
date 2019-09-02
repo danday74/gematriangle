1a76205e26ab10129a04e13a40ef2f6914d0ef91
Daniel Lewis <daniellewis777@gmail.com>
2019-09-02 02:15:09 +0100

    table to triangle link works

--- src/app/routes/triangles/triangles.component.ts
+++ src/app/routes/triangles/triangles.component.ts
@@ -4,6 +4,7 @@ import { TrianglesService } from './triangles.service'
 import { filter, takeUntil } from 'rxjs/operators'
 import { StorageService } from '../../services/storage.service'
 import { TriangleToolboxMessage } from './triangle-toolbox/triangle-toolbox-message.enum'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-triangles',
@@ -15,11 +16,19 @@ export class TrianglesComponent extends DestroyerComponent implements OnInit {
 
   rowCount: number
 
-  constructor(private trianglesService: TrianglesService, private storageService: StorageService) {
+  constructor(private route: ActivatedRoute, private trianglesService: TrianglesService, private storageService: StorageService) {
     super()
   }
 
   ngOnInit() {
+
+    let routeRowCount = this.route.snapshot.params.id
+
+    if (routeRowCount) {
+      routeRowCount = parseInt(routeRowCount, 10)
+      this.storageService.setItem('row-count', routeRowCount)
+    }
+
     this.rowCount = this.storageService.getItem('row-count') || 37
 
     this.trianglesService.triangleToolboxMessage$.pipe(
