d08ad3179101b419350b0a57d00ddb06f6a534b7
Daniel Lewis <daniellewis777@gmail.com>
2019-09-02 15:17:32 +0100

    titles

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getTitleText()).toEqual('gematria app is running!')
+    expect(page.getTitleText()).toEqual('gematriangle app is running!')
   })
 
   afterEach(async () => {
