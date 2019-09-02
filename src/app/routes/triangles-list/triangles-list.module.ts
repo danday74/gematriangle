9de0c01f8e90cdb02502766a765d86f61840dcdc
Daniel Lewis <daniellewis777@gmail.com>
2019-09-02 01:03:59 +0100

    lint

--- src/app/routes/triangles-list/triangles-list.module.ts
+++ src/app/routes/triangles-list/triangles-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { TrianglesListRoutingModule } from './triangles-list-routing.module';
+import { TrianglesListRoutingModule } from './triangles-list-routing.module'
 import { TrianglesListComponent } from './triangles-list.component'
 
 @NgModule({
